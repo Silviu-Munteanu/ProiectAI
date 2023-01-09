cd2ab04647eaf3f675c8e32bdcd2d96d6f4bee99
Silviu-Munteanu <79187810+Silviu-Munteanu@users.noreply.github.com>
2023-01-09 20:47:06 +0200

    Sentence similarity accounts for size and displacement of words, class parameters for severity. Split for ? and ! , word similarity adjustment

--- SimilarityAnalyser.py
+++ SimilarityAnalyser.py
@@ -8,21 +8,23 @@ from Tagger import Tagger
 
 
 class SimilarityAnalyser:
-    def __init__(self, text1, text2):
+    def __init__(self, text1, text2, size_penalty=0.2, displacement_penalty=0):
         self.text1 = text1
         self.text2 = text2
+        self.size_penalty = size_penalty
+        self.displacement_penalty = displacement_penalty
 
     @staticmethod
     def __create_list_of_sentences_from_text(text: str):
-        return re.split("[.;]", text)
+        return re.split("[.;!?]", text)
 
     @staticmethod
     def __get_sentence_from_index(text, index):
-        return re.split("[.;]", text)[index]
+        return re.split("[.;!?]", text)[index]
 
     @staticmethod
     def __get_sentences_from_tuple(tupla, text1, text2):
-        return re.split("[.;]", text1)[tupla[0]], re.split("[.;]", text2)[tupla[1]]
+        return re.split("[.;!?]", text1)[tupla[0]], re.split("[.;!?]", text2)[tupla[1]]
 
     @staticmethod
     def __get_word_similarity(word1, part1, word2, part2):
@@ -30,63 +32,74 @@ class SimilarityAnalyser:
         synset2 = wn.synsets(word2, part2)
         if synset1 and synset2:
             return wn.wup_similarity(synset1[0], synset2[0])
-        return 0
+        elif word1 == word2:
+            return 1
+        else:
+            return 0
 
     @staticmethod
-    def __get_skewed_sentence_similarity(sentence1, tags1, sentence2, tags2):
+    def __get_skewed_sentence_similarity(sentence1, tags1, sentence2, tags2, size_penalty, displacement_penalty):
         l1 = len(sentence1)
         l2 = len(sentence2)
+        max_distance = max(l1, l2) - 1
         sum = 0
         for i in range(l1):
             best_match = 0
             for j in range(l2):
-                sim = SimilarityAnalyser.__get_word_similarity(sentence1[i], tags1[i], sentence2[j], tags2[j])
+                sim = SimilarityAnalyser.__get_word_similarity(sentence1[i], tags1[i], sentence2[j], tags2[j]) / (
+                            1 + displacement_penalty * (abs(i - j) / max_distance))
                 if best_match < sim:
                     best_match = sim
             sum += best_match
-        return sum / l1
+        size_discrepancy = abs(l1 - l2)
+        size_discrepancy_ratio = size_discrepancy / min(l1, l2)
+        return sum / l1 / (1 + size_discrepancy_ratio * size_penalty)
 
     @staticmethod
-    def __get_basic_sentence_similarity(sentence1, tags1, sentence2, tags2):
-        return (SimilarityAnalyser.__get_skewed_sentence_similarity(sentence1, tags1, sentence2, tags2)
+    def __get_sentence_similarity(sentence1, tags1, sentence2, tags2, size_penalty, displacement_penalty):
+        return (SimilarityAnalyser.__get_skewed_sentence_similarity(sentence1, tags1, sentence2, tags2, size_penalty,
+                                                                    displacement_penalty)
                 + SimilarityAnalyser.__get_skewed_sentence_similarity(sentence2, tags2, sentence1,
-                                                                      tags1)) / 2  # the arithmetic mean
+                                                                      tags1, size_penalty,
+                                                                      displacement_penalty)) / 2  # the arithmetic mean
 
-    @staticmethod
-    def __get_skewed_text_similarity(list_of_sentences1, roles_of_sentences1, list_of_sentences2, roles_of_sentences2):
+    def __get_skewed_text_similarity(self, list_of_sentences1, roles_of_sentences1, list_of_sentences2,
+                                     roles_of_sentences2):
         l1 = len(list_of_sentences1)
         l2 = len(list_of_sentences2)
         sum = 0
         for i in range(l1):
             best_match = 0
             for j in range(l2):
-                sentence_similarity = SimilarityAnalyser.__get_basic_sentence_similarity(list_of_sentences1[i],
-                                                                                         roles_of_sentences1[i],
-                                                                                         list_of_sentences2[j],
-                                                                                         roles_of_sentences2[j])
+                sentence_similarity = SimilarityAnalyser.__get_sentence_similarity(list_of_sentences1[i],
+                                                                                   roles_of_sentences1[i],
+                                                                                   list_of_sentences2[j],
+                                                                                   roles_of_sentences2[j],
+                                                                                   self.size_penalty,
+                                                                                   self.displacement_penalty)
                 if best_match < sentence_similarity:
                     best_match = sentence_similarity
             sum += best_match
         return sum / l1  # the arithmetic mean of the similarities
 
-    @staticmethod
-    def get_semantic_distance_matrix(list_of_sentences1, roles_of_sentences1, list_of_sentences2,
+    def get_semantic_distance_matrix(self, list_of_sentences1, roles_of_sentences1, list_of_sentences2,
                                      roles_of_sentences2):
         output = []
         for i in range(len(list_of_sentences1)):
             temp = []
             for j in range(len(list_of_sentences2)):
                 temp.append(
-                    SimilarityAnalyser.__get_basic_sentence_similarity(list_of_sentences1[i], roles_of_sentences1[i],
-                                                                       list_of_sentences2[j], roles_of_sentences2[j]))
+                    SimilarityAnalyser.__get_sentence_similarity(list_of_sentences1[i], roles_of_sentences1[i],
+                                                                 list_of_sentences2[j],
+                                                                 roles_of_sentences2[j], self.size_penalty,
+                                                                 self.displacement_penalty))
             output.append(temp)
         return output
 
-    @staticmethod
-    def show_semantic_distance_matrix_heatmap(text1, text2):
+    def show_semantic_distance_matrix_heatmap(self, text1, text2):
         list_of_sentences1, roles_of_sentences1 = SimilarityAnalyser.__get_words_and_their_roles_from_text(text1)
         list_of_sentences2, roles_of_sentences2 = SimilarityAnalyser.__get_words_and_their_roles_from_text(text2)
-        matrix = np.array(SimilarityAnalyser.get_semantic_distance_matrix(list_of_sentences1,
+        matrix = np.array(SimilarityAnalyser.get_semantic_distance_matrix(self, list_of_sentences1,
                                                                           roles_of_sentences1,
                                                                           list_of_sentences2,
                                                                           roles_of_sentences2))
@@ -107,7 +120,7 @@ class SimilarityAnalyser:
     def average_distance_all_cases(self):
         list_of_sentences1, roles_of_sentences1 = SimilarityAnalyser.__get_words_and_their_roles_from_text(self.text1)
         list_of_sentences2, roles_of_sentences2 = SimilarityAnalyser.__get_words_and_their_roles_from_text(self.text2)
-        matrix = SimilarityAnalyser.get_semantic_distance_matrix(list_of_sentences1,
+        matrix = SimilarityAnalyser.get_semantic_distance_matrix(self, list_of_sentences1,
                                                                  roles_of_sentences1,
                                                                  list_of_sentences2,
                                                                  roles_of_sentences2)
@@ -125,10 +138,11 @@ class SimilarityAnalyser:
         temp = {}
         for i in range(len(list_of_sentences1)):
             for j in range(len(list_of_sentences2)):
-                temp[(i, j)] = SimilarityAnalyser.__get_basic_sentence_similarity(list_of_sentences1[i],
-                                                                                  roles_of_sentences1[i],
-                                                                                  list_of_sentences2[j],
-                                                                                  roles_of_sentences2[j])
+                temp[(i, j)] = SimilarityAnalyser.__get_sentence_similarity(list_of_sentences1[i],
+                                                                            roles_of_sentences1[i],
+                                                                            list_of_sentences2[j],
+                                                                            roles_of_sentences2[j], self.size_penalty,
+                                                                            self.displacement_penalty)
         sorted_list = dict(sorted(temp.items(), key=lambda item: item[1], reverse=True))
         matches = []
         contor = [i for i in range(
@@ -150,7 +164,7 @@ class SimilarityAnalyser:
     def closest_to_similarity_score(self, prag):
         list_of_sentences1, roles_of_sentences1 = SimilarityAnalyser.__get_words_and_their_roles_from_text(self.text1)
         list_of_sentences2, roles_of_sentences2 = SimilarityAnalyser.__get_words_and_their_roles_from_text(self.text2)
-        matrix = SimilarityAnalyser.get_semantic_distance_matrix(list_of_sentences1,
+        matrix = SimilarityAnalyser.get_semantic_distance_matrix(self, list_of_sentences1,
                                                                  roles_of_sentences1,
                                                                  list_of_sentences2,
                                                                  roles_of_sentences2)
@@ -158,7 +172,8 @@ class SimilarityAnalyser:
         for i in matrix:
             for j in i:
                 if j < prag:
-                    output[(matrix.index(i), i.index(j))] = j, SimilarityAnalyser.__get_sentences_from_tuple((matrix.index(i), i.index(j)), self.text1, self.text2)
+                    output[(matrix.index(i), i.index(j))] = j, SimilarityAnalyser.__get_sentences_from_tuple(
+                        (matrix.index(i), i.index(j)), self.text1, self.text2)
         return dict(sorted(output.items(), key=lambda item: item[1], reverse=True))
 
     @staticmethod
@@ -171,18 +186,18 @@ class SimilarityAnalyser:
                                            list_of_list_of_words1]
         return list_of_list_of_words1, list_of_list_of_roles_of_words1
 
-    def get_basic_text_similarity(self):
+    def get_text_similarity(self):
         list_of_list_of_words1, list_of_list_of_roles_of_words1 = SimilarityAnalyser.__get_words_and_their_roles_from_text(
             self.text1)
         list_of_list_of_words2, list_of_list_of_roles_of_words2 = SimilarityAnalyser.__get_words_and_their_roles_from_text(
             self.text2)
-        print(list_of_list_of_words1)  # FOR DEBUG PURPOSE
-        print(list_of_list_of_roles_of_words1)  # FOR DEBUG PURPOSE
-        text1_text2_similarity = SimilarityAnalyser.__get_skewed_text_similarity(list_of_list_of_words1,
+        # print(list_of_list_of_words1)  # FOR DEBUG PURPOSE
+        # print(list_of_list_of_roles_of_words1)  # FOR DEBUG PURPOSE
+        text1_text2_similarity = SimilarityAnalyser.__get_skewed_text_similarity(self, list_of_list_of_words1,
                                                                                  list_of_list_of_roles_of_words1,
                                                                                  list_of_list_of_words2,
                                                                                  list_of_list_of_roles_of_words2)
-        text2_text1_similarity = SimilarityAnalyser.__get_skewed_text_similarity(list_of_list_of_words2,
+        text2_text1_similarity = SimilarityAnalyser.__get_skewed_text_similarity(self, list_of_list_of_words2,
                                                                                  list_of_list_of_roles_of_words2,
                                                                                  list_of_list_of_words1,
                                                                                  list_of_list_of_roles_of_words1)
