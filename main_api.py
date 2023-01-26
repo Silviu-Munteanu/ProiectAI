56dcc1793c4f1c2af6990f10384714fba2486b5d
Nae Theodor-Gabriel <73588948+ntheodorg@users.noreply.github.com>
2023-01-26 23:07:41 +0200

    Finishing additional stats info design

--- main_api.py
+++ main_api.py
@@ -21,13 +21,15 @@ def compare():
 
     similarity_analyzer = SimilarityAnalyser(data['text_1'], data['text_2'], sp, dp)
     stat1 = (int(similarity_analyzer.average_distance_all_cases()),
-             "After computing the distance between every pair of sentences, returns the average.")
+             "After computing the distance between every pair of sentences, returns the average.",
+             "Average distance")
     stat2 = (int(similarity_analyzer.average_distance_after_greedy_assignation()[0]),
              "Computes the similarity scores between all posible sentences in "
              "the 2 pieces of text. Based on these scores, it maches the pair(one "
              "from the first text, the second one from the second text) with the "
              "best score and removes them from the pool. This process repeats "
-             "until the maximum number of matches is achieved. Returns the average of these assignations")
+             "until the maximum number of matches is achieved. Returns the average of these assignations",
+             "Average distance after greedy assignation")
     stat4 = (int(similarity_analyzer.get_text_similarity()),
              "Minimum distance: Computes the average between the best matches "
              "of the first sentence in the second and the best matches of the "
@@ -35,7 +37,8 @@ def compare():
              "sentences of different word counts (proportionate to the "
              "size of the shortest sentence), displacement penalty: "
              "penalizes sentences with different ordering of words "
-             "based on the distance between the indexes of words inside the sentences.")
+             "based on the distance between the indexes of words inside the sentences.",
+             "Text Similarity")
     # stat3 = int(similarity_analyzer.closest_to_similarity_score(0.4)
     additional_stats = [stat1, stat2, stat4]
 
